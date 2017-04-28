bf5851b38092850b7fe34453f043539dfd7e52e7
Jonathan Cronin <cronin_jonathan@bah.com>
2017-04-28 13:34:33 -0400

    second video

--- js/main.js
+++ js/main.js
@@ -1,22 +1,34 @@
 $(document).ready(function(){
+	var videos=[
+	{
+		filename: "bunny.mp4",
+		url: "https://github.com/mediaelement/mediaelement-files/raw/master/big_buck_bunny.mp4"
+	},
+	{
+		filename: "oceans.mp4",
+		url: "http://vjs.zencdn.net/v/oceans.mp4"
+	}];
 
 	$('.btn-save').click(function(){
-		var filename = "test.mp4";
-		var videoURL = "https://github.com/mediaelement/mediaelement-files/raw/master/big_buck_bunny.mp4";
+		$.each(videos,function(){
+			var video=this;
 
-		requestFileSystem(PERSISTENT, 0, function(fileSystem) {
-		    var ft = new FileTransfer();
-		    ft.download(videoURL, fileSystem.root.toURL() + "/" + filename, function(entry) {
-		    	localStorage.setItem('videourl',fileSystem.root.toURL() + "/" + filename);
-		    	console.log('videourl',fileSystem.root.toURL() + "/" + filename);
-		    	console.log(entry);
+			requestFileSystem(PERSISTENT, 0, function(fileSystem) {
+			    var ft = new FileTransfer();
+			    ft.download(video.url, fileSystem.root.toURL() + "/" + video.filename, function(entry) {
+			    	
+			    	localStorage.setItem('videourl',fileSystem.root.toURL() + "/" + video.filename);
+			    	console.log('videourl',fileSystem.root.toURL() + "/" + video.filename);
+			    	console.log(entry);
 
-		        var videoElement = document.createElement('video');
-		        videoElement.controls = 'controls';
-		        videoElement.src = entry;
-		        document.getElementById("video").appendChild(videoElement);
-		    });
+			        var videoElement = document.createElement('video');
+			        videoElement.controls = 'controls';
+			        videoElement.src = localStorage.videourl;
+			        document.getElementById("video").appendChild(videoElement);
+			    });
+			});
 		});
+
 	});
 
 	$('.btn-play').click(function(){
