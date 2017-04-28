9dd83a9a05d478e7aa3d5996f3b480ddd6218c66
Jonathan Cronin <cronin_jonathan@bah.com>
2017-04-28 14:19:04 -0400

    more realistic example

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-	var videos=[
+var videos;
+var videosStarter=[
 	{
 		filename: "bunny.mp4",
 		url: "https://github.com/mediaelement/mediaelement-files/raw/master/big_buck_bunny.mp4"
@@ -9,32 +9,82 @@ $(document).ready(function(){
 		url: "http://vjs.zencdn.net/v/oceans.mp4"
 	}];
 
-	$('.btn-save').click(function(){
-		$.each(videos,function(){
-			var video=this;
+//if local version, use that, if not, begin with starter
+if(localStorage.videos){
+	videos=JSON.parse(localStorage.videos);
+}
+else{
+	localStorage.videos=videosStarter;
+}
 
-			requestFileSystem(PERSISTENT, 0, function(fileSystem) {
-			    var ft = new FileTransfer();
-			    ft.download(video.url, fileSystem.root.toURL() + "/" + video.filename, function(entry) {
-			    	
-			    	localStorage.setItem('videourl',fileSystem.root.toURL() + "/" + video.filename);
-			    	console.log('videourl',fileSystem.root.toURL() + "/" + video.filename);
-			    	console.log(entry);
 
-			        var videoElement = document.createElement('video');
-			        videoElement.controls = 'controls';
-			        videoElement.src = localStorage.videourl;
-			        document.getElementById("video").appendChild(videoElement);
-			    });
-			});
+function updateLocal(){
+	localStorage.videos=JSON.stringify(videos);
+}
+
+
+
+function populateButtons(){
+	$.each(videos,function(index){
+		var localString='';
+		var newButton=$('<a href="'+this.url+'" data-index="'+this.index+'" class="btn btn-primary">'+localString+this.filename+'</a>');
+
+		if(this.local){
+			newButton.before('LOCAL: ');
+			newButton.addClass('local').css('background','red');
+		}
+
+		newButton.appendTo('#buttons');
+	});
+}
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady(){
+
+	$(document).ready(function(){
+		
+
+		
+
+
+		$('#buttons').on('click','btn',function(){
+
+			var videoBtn=$(this);
+			var video=videos[videoBtn.attr('data-index')];
+
+			if(navigator.online){
+
+				requestFileSystem(PERSISTENT, 0, function(fileSystem) {
+				    var ft = new FileTransfer();
+				    var localURL=fileSystem.root.toURL() + "/" + video.filename;
+				    console.log('downloading: '+video.url);
+
+				    ft.download(video.url, localURL, function(entry) {
+				    	console.log('downloaded: '+video.url);
+				    	video.local=localURL;
+				    	
+				    	updateLocal();
+				        $('#buttons').empty();
+				        populateButtons();
+
+				        var videoElement = $('<video controls src="'+loclalURL+'"></video>');
+				        $('#video').empty();
+				        $('#video').append(videoElement);
+				    });
+				});
+			}
+			if(videoBtn.hasClass('local')){
+				var videoElement = $('<video controls src="'+video.local+'"></video>');
+		        $('#video').empty();
+		        $('#video').append(videoElement);
+			}
+			else{
+				alert('not local video');
+			}
+
+			return false;
 		});
 
 	});
 
-	$('.btn-play').click(function(){
-		var videoElement = document.createElement('video');
-        videoElement.controls = 'controls';
-        videoElement.src = localStorage.videourl;
-        document.getElementById("video").appendChild(videoElement);
-	})
-});
+}
