68143053e0254c9d4af1b394f77120651a4c9466
Jonathan Cronin <cronin_jonathan@bah.com>
2017-04-28 10:59:47 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+	$('.btn-save').click(function(){
+		var filename = "test.mp4";
+		var videoURL = "https://github.com/mediaelement/mediaelement-files/raw/master/big_buck_bunny.mp4";
+
+		requestFileSystem(PERSISTENT, 0, function(fileSystem) {
+		    var ft = new FileTransfer();
+		    ft.download(videoURL, fileSystem.root.toURL() + "/" + filename, function(entry) {
+		    	localStorage.setItem('videourl',fileSystem.root.toURL() + "/" + filename);
+		    	console.log('videourl',fileSystem.root.toURL() + "/" + filename);
+		    	console.log(entry);
+
+		        var videoElement = document.createElement('video');
+		        videoElement.controls = 'controls';
+		        videoElement.src = entry;
+		        document.videoElementById("video").appendChild(imgElement);
+		    });
+		});
+	});
+
+	$('.btn-play').click(function(){
+		var videoElement = document.createElement('video');
+        videoElement.controls = 'controls';
+        videoElement.src = localStorage.videourl;
+        document.videoElementById("video").appendChild(imgElement);
+	})
+});
